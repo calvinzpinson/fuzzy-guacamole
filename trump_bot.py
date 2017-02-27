b3a095a3341cee7fdcecaa20b0a8d7ad41f93504
Calvin <calvinzpinson@gmail.com>
2017-02-26 19:51:38 -0800

    Twitter postin basics

---
+++ trump_bot.py
@@ -0,0 +1,33 @@
+import ConfigParser
+import tweepy
+
+def get_config():
+    config_parser = ConfigParser.ConfigParser()
+    config_path = "./config"
+    config_parser.read(config_path)
+
+    return config_parser
+
+def get_api(cfg):
+    auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+    auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+
+    return tweepy.API(auth)
+
+def tweet(message):
+    config = get_config()
+    cfg = {
+        'consumer_key'          : config.get('Twitter', 'consumer_key'),
+        'consumer_secret'       : config.get('Twitter', 'consumer_secret'),
+        'access_token'          : config.get('Twitter', 'access_token'),
+        'access_token_secret'   : config.get('Twitter', 'access_token_secret')
+    }
+
+    api = get_api(cfg)
+    api.update_status(status=message)
+
+def main():
+    tweet('Hello world!')
+
+if __name__ == '__main__':
+    main()
